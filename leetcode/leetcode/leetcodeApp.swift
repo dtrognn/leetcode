12032075953487e2e76f829cf9a27df0abfffdf7
dtrognn <dtrognn@gmail.com>
2025-05-10 11:33:16 +0700

    initial project

---
+++ leetcode/leetcode/leetcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  leetcodeApp.swift
+//  leetcode
+//
+//  Created by dtrognn on 10/5/25.
+//
+
+import SwiftUI
+
+@main
+struct leetcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
